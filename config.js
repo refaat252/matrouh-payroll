7df8ca4a6af54c53393dc7bc12a06423c5e4f63d
refaat252 <109837051+refaat252@users.noreply.github.com>
2026-06-21 17:01:14 +0300

    Update API_URL to new deployment link

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - إعدادات التطبيق
 export const CONFIG = {
     // ضع هنا رابط التطبيق الجديد الذي نشرته في Apps Script
-    API_URL: 'https://script.google.com/macros/s/Your_New_Deployment_ID/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbzY_p8XZdOeCbIAyd0pKfL9w5T3wk8z5J-fm2AF6PUwHaUSRJV747_m6qSFnAbysHyLjA/exec',
     // المفتاح السري (يجب أن يتطابق مع الموجود في الكود الخلفي)
     API_KEY: 'XyZ@2025!Secure',
     SHEET_NAME: 'Sheet1'
