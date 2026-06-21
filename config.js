39fa2dc052120876a9438f8d3ee88aa89070a995
refaat252 <109837051+refaat252@users.noreply.github.com>
2026-06-21 16:19:36 +0300

    Add files via upload
    جزء البحث عن طريق السكريبت

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js - إعدادات التطبيق
+export const CONFIG = {
+    // ضع هنا رابط التطبيق الجديد الذي نشرته في Apps Script
+    API_URL: 'https://script.google.com/macros/s/Your_New_Deployment_ID/exec',
+    // المفتاح السري (يجب أن يتطابق مع الموجود في الكود الخلفي)
+    API_KEY: 'XyZ@2025!Secure',
+    SHEET_NAME: 'Sheet1'
+};
