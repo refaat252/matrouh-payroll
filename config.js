ecd949a8c761e9aec85f312e6f3642c914cfb738
refaat252 <109837051+refaat252@users.noreply.github.com>
2026-07-06 13:10:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - إعدادات التطبيق
 export const CONFIG = {
     // رابط Google Apps Script Web App
-    API_URL: 'https://script.google.com/macros/s/AKfycbzY_p8XZdOeCbIAyd0pKfL9w5T3wk8z5J-fm2AF6PUwHaUSRJV747_m6qSFnAbysHyLjA/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbwqcc5MhQtWMhg-LrSFr4zLU5r2qww0WgWiLFKgY7zCZ-azCtJzkDhY_WaE4toCopIh/exec',
 
     // المفتاح السري (يجب أن يتطابق مع الموجود في الكود الخلفي)
     API_KEY: 'XyZ@2025!Secure'
