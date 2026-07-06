725c1fd2f8363de97a3bcd8e16c3710cfd4d2b5e
refaat252 <109837051+refaat252@users.noreply.github.com>
2026-07-06 14:01:55 +0300

    test kimi Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - إعدادات التطبيق
 export const CONFIG = {
     // رابط Google Apps Script Web App
-    API_URL: 'https://script.google.com/macros/s/AKfycbzurDkPoMrZFWM7WyXvird38IsNRWrm3ThNbVmrwGpwZewcHEbW6ciNDRa9I4okIXWvPQ/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbztZXccO_cDgD57zSPrt5sZT_6r36va7eSXnXIDZiColDdON3lAZ-OidOTnU2JRYL-onA/exec',
 
     // المفتاح السري (يجب أن يتطابق مع الموجود في الكود الخلفي)
     API_KEY: 'XyZ@2025!Secure'
